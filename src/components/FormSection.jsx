1d3d96df336077240465750cdb633a075ae19f6d
Hakan Yalniz <hakan_yalniz@live.com>
2023-09-04 15:35:22 +0300

    Now the Forms component creates as many components as required

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -3,18 +3,20 @@ import Forms from "./Forms.jsx";
 
 function FormSection() {
   const personalInformation = [
-    "Personal Information",
-    { firstInput: "First name", htmlFor: "fname", type: "text" },
-    { secondInput: "Last name", htmlFor: "lname", type: "text" },
-    { thirdInput: "E-mail", htmlFor: "email", type: "email" },
-    { fourthInput: "Phone", htmlFor: "phone", type: "number" },
+    { labelContent: "First name", htmlFor: "fname", type: "text" },
+    { labelContent: "Last name", htmlFor: "lname", type: "text" },
+    { labelContent: "E-mail", htmlFor: "email", type: "email" },
+    { labelContent: "Phone", htmlFor: "phone", type: "number" },
   ];
   return (
     <div className="form-section">
       <div className="introduction"></div>
       <div className="forms">
-        <Forms personalInformation={personalInformation} />
-        <Forms personalInformation={personalInformation} />
+        <Forms
+          personalInformation={personalInformation}
+          legend="Personal Information"
+        />
+        <Forms personalInformation={personalInformation} legend="Experience" />
       </div>
     </div>
   );
--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,50 +1,23 @@
-function Forms({ personalInformation }) {
-  const [legendName, firstObj, secondObj, thirdObj, fourthObj] =
-    personalInformation;
-
+function Forms({ personalInformation, legend }) {
   return (
     <form>
       <fieldset>
-        <legend>{legendName}</legend>
-        <div>
-          <label htmlFor={firstObj.htmlFor}>{firstObj.firstInput}</label>
-          <br />
-          <input
-            type={firstObj.type}
-            id={firstObj.htmlFor}
-            name={firstObj.htmlFor}
-          />
-        </div>
-
-        <div>
-          <label htmlFor={secondObj.htmlFor}>{secondObj.secondInput}</label>
-          <br />
-          <input
-            type={secondObj.type}
-            id={secondObj.htmlFor}
-            name={secondObj.htmlFor}
-          />
-        </div>
-
-        <div>
-          <label htmlFor={thirdObj.htmlFor}>{thirdObj.thirdInput}</label>
-          <br />
-          <input
-            type={thirdObj.type}
-            id={thirdObj.htmlFor}
-            name={thirdObj.htmlFor}
-          />
-        </div>
-
-        <div>
-          <label htmlFor={fourthObj.htmlFor}>{fourthObj.fourthInput}</label>
-          <br />
-          <input
-            type={fourthObj.type}
-            id={fourthObj.htmlFor}
-            name={fourthObj.htmlFor}
-          />
-        </div>
+        <legend>{legend}</legend>
+        {personalInformation.map((inputElement) => {
+          return (
+            <div>
+              <label htmlFor={inputElement.htmlFor}>
+                {inputElement.labelContent}
+              </label>
+              <br />
+              <input
+                type={inputElement.type}
+                id={inputElement.htmlFor}
+                name={inputElement.htmlFor}
+              />
+            </div>
+          );
+        })}
       </fieldset>
     </form>
   );
