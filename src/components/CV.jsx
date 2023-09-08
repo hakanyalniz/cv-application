61ad4885d93acf691b45d51990acfc4849aa583e
Hakan Yalniz <hakan_yalniz@live.com>
2023-09-08 15:26:10 +0300

    Finished the basics of CV CSS

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -69,20 +69,62 @@ function CV({ firstName }) {
           natus quam nihil, quasi perferendis illo quae est expedita dolor nam?
         </div>
 
-        <hr />
-
         <div className="experience">
-          <div className="title">Experience</div>
-          <div className="experience-1">Example</div>
-          <div className="experience-2">Example</div>
-          <div className="experience-3">Example</div>
+          <div className="title sub-title">Experience</div>
+          <div className="experience-grid">
+            <div className="experience-date">2010-2020</div>
+            <div className="experience-details">
+              <div className="title">Computer Scientist</div>
+              <div className="title-location">Ipsum Computers</div>
+              <ul>
+                <li>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  architecto?
+                </li>
+                <li>
+                  Repellendus quod dicta nostrum hic neque at doloremque soluta
+                </li>
+                <li>
+                  consectetur perspiciatis, libero vel reiciendis. Voluptatibus
+                </li>
+                <li>laboriosam vel iusto praesentium dolorum blanditiis</li>
+              </ul>
+            </div>
+
+            <div className="experience-date">2020-Present</div>
+            <div className="experience-details">
+              <div className="title">Engineer</div>
+              <div className="title-location">Lorem Engineer</div>
+              <ul>
+                <li>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  architecto?
+                </li>
+                <li>
+                  Repellendus quod dicta nostrum hic neque at doloremque soluta
+                </li>
+                <li>
+                  consectetur perspiciatis, libero vel reiciendis. Voluptatibus
+                </li>
+                <li>laboriosam vel iusto praesentium dolorum blanditiis</li>
+              </ul>
+            </div>
+          </div>
         </div>
 
-        <hr />
-
         <div className="education">
-          <div className="title">Education</div>
-          <div className="education-1"></div>
+          <div className="title sub-title">Education</div>
+          <div className="education-grid">
+            <div className="education-date">1990-2000</div>
+            <div className="education-details">
+              <div className="title">Masters in Lorem</div>
+              <div className="title-location">Lorem School of Ipsum</div>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod
+              expedita neque nihil natus possimus quas pariatur ipsum, sequi
+              sunt et, blanditiis incidunt modi cupiditate nam magni quidem
+              quae! Neque, vitae?
+            </div>
+          </div>
         </div>
       </div>
     </div>
